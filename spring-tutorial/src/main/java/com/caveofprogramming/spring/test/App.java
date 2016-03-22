7b1882bffe70d3d70b45c7cdb917ab9f6e87397d
Rahul John Louis <rahul.louis@gmail.com>
2016-03-21 21:00:28 -0400

    Initial commit

---
+++ spring-tutorial/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -0,0 +1,24 @@
+package com.caveofprogramming.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/caveofprogramming/spring/test/beans/beans.xml");
+
+		Person person1 = (Person) context.getBean("person");
+		//person1.speak();
+		Person person2 = (Person) context.getBean("person");
+		
+		Address address = (Address) context.getBean("address");
+		person1.setTaxId(32222);
+		System.out.println(person1);
+		System.out.println(person2);
+		System.out.println(address);
+		((ClassPathXmlApplicationContext) context).close();
+	}
+}
