dc644196177232935463ae51d85f18ab8528b058
Rahul John Louis <rahul.louis@gmail.com>
2016-03-21 21:05:52 -0400

    refactor

--- spring-tutorial/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -16,6 +16,7 @@ public class App {
 		
 		Address address = (Address) context.getBean("address");
 		person1.setTaxId(32222);
+		
 		System.out.println(person1);
 		System.out.println(person2);
 		System.out.println(address);
